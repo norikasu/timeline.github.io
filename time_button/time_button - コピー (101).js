9ed731d1f18a66532454de5d37a6178f848b1030
norikasu <58767570+norikasu@users.noreply.github.com>
2019-12-11 19:08:34 +0900

    Add files via upload

---
+++ time_button/time_button - コピー (101).js
@@ -0,0 +1,9 @@
+function exec101(){
+  var today=new Date();//今日の日付を取得
+  var ToDay=(today.getMonth()+1)+"/"+today.getDate()+" "+today.getHours()+":"+today.getMinutes();//表示する内容
+  document.getElementById("output101").innerHTML=ToDay;//指定した場所に表示させる
+  }
+
+  function changeBoxColor34( newColor ) {
+    document.getElementById('b005').style.backgroundColor = newColor;
+  }
