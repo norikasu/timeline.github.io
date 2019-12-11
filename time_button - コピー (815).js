b9f705b2be139c6ab03c20be2620af979e9ff768
norikasu <58767570+norikasu@users.noreply.github.com>
2019-12-11 18:22:07 +0900

    Delete time_button - コピー (815).js

--- time_button - コピー (815).js
+++
@@ -1,9 +0,0 @@
-function exec815(){
-  var today=new Date();//今日の日付を取得
-  var ToDay=(today.getMonth()+1)+"/"+today.getDate()+" "+today.getHours()+":"+today.getMinutes();//表示する内容
-  document.getElementById("output815").innerHTML=ToDay;//指定した場所に表示させる
-  }
-
-function changeBoxColor264( newColor ) {
-  document.getElementById('e035').style.backgroundColor = newColor;
-  }
