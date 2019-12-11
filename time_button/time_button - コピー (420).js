9218f6b36218d3f43f1fdd19dbcfdb5bfc5a69ad
norikasu <58767570+norikasu@users.noreply.github.com>
2019-12-11 18:53:54 +0900

    Add files via upload

---
+++ time_button/time_button - コピー (420).js
@@ -0,0 +1,9 @@
+function exec420(){
+  var today=new Date();//今日の日付を取得
+  var ToDay=(today.getMonth()+1)+"/"+today.getDate()+" "+today.getHours()+":"+today.getMinutes();//表示する内容
+  document.getElementById("output420").innerHTML=ToDay;//指定した場所に表示させる
+  }
+
+  function changeBoxColor136( newColor ) {
+    document.getElementById('a019').style.backgroundColor = newColor;
+  }
---
+++ time_button/time_button - コピー (498).js
@@ -0,0 +1,9 @@
+function exec498(){
+  var today=new Date();//今日の日付を取得
+  var ToDay=(today.getMonth()+1)+"/"+today.getDate()+" "+today.getHours()+":"+today.getMinutes();//表示する内容
+  document.getElementById("output498").innerHTML=ToDay;//指定した場所に表示させる
+  }
+
+    function changeBoxColor160( newColor ) {
+      document.getElementById('b022').style.backgroundColor = newColor;
+    }
