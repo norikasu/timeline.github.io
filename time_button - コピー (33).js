71f306cd02360dc24e423bc63ead39f369018fea
norikasu <58767570+norikasu@users.noreply.github.com>
2020-01-09 14:39:53 +0900

    Delete time_button - コピー (33).js

--- time_button - コピー (33).js
+++
@@ -1,9 +0,0 @@
-function exec33(){
-  var today=new Date();//今日の日付を取得
-  var ToDay=(today.getMonth()+1)+"/"+today.getDate()+" "+today.getHours()+":"+today.getMinutes();//表示する内容
-  document.getElementById("output33").innerHTML=ToDay;//指定した場所に表示させる
-  }
-
-  function changeBoxColor11( newColor ) {
-    document.getElementById('c002').style.backgroundColor = newColor;
-  }
